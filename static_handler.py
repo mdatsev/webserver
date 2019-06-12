2b1249cc12a1686bdd7de7a268704ee397bf24fe
mdatsev <martin.datsev.p@gmail.com>
2019-06-12 13:27:56 +0300

    implemented proper path translation

--- static_handler.py
+++ static_handler.py
@@ -1,14 +1,29 @@
 from pathlib import Path
+import urllib
 
 ROOT_DIR = Path.cwd()
 
-def get_path(uri):
-    # todo
-    return ROOT_DIR.joinpath(normalize_uri(uri).strip('/'))
-
 def normalize_uri(uri):
-    # todo https://en.wikipedia.org/wiki/URL_normalization
-    return uri
+    uri = uri.lower()
+    uri = urllib.parse.urlparse(uri)
+    return uri._replace(path=urllib.parse.unquote(uri.path))
+
+def remove_dot_segments(path):
+    input = path.strip('/').split('/')
+    output = []
+    for seg in input:
+        if seg == '.':
+            continue
+        if seg == '..':
+            if(len(output) > 0):
+                output.pop()
+            continue
+        output.append(seg)
+    return output
+
+def get_path(uri):
+    uri = normalize_uri(uri)
+    return ROOT_DIR.joinpath(ROOT_DIR, *remove_dot_segments(uri.path))
 
 def handler(request):
     path = get_path(request.uri)
