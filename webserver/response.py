d160bf768ecfca3a428bc2ffad525981f6cc1cf9
mdatsev <martin.datsev.p@gmail.com>
2019-07-04 16:19:07 +0300

    streaming response

--- webserver/response.py
+++ webserver/response.py
@@ -38,3 +38,40 @@ class HTTPResponse:
             return
         self.headers['content-encoding'] = encoding
         return self
+
+class HTTPStreamResponse:
+    def __init__(self, http_version, writer):
+        self.http_version = http_version
+        self.writer = writer
+        self.status_written = False
+        self.headers_written = False
+
+    async def write(self, data):
+        self.writer.write(data)
+        await self.writer.drain()
+
+    async def write_status(self, status):
+        assert(not self.status_written)
+        await self.write(bytes(self.http_version + ' ' + status + '\r\n', 'ascii'))
+        self.status_written = True
+
+    async def write_headers(self, headers):
+        assert(self.status_written)
+        assert(not self.headers_written)
+        raw = b''
+        for k, v in headers.items():
+            if isinstance(v, str):
+                v = bytes(v, 'ascii')
+            raw += bytes(k + ': ', 'ascii') + v + b'\r\n'
+        await self.write(raw)
+
+    async def write_body(self, body):
+        if not self.headers_written:
+            await self.write(b'\r\n')
+            self.headers_written = True
+        await self.write(body)
+
+    async def send(self, status, headers, body):
+        await self.write_status(status)
+        await self.write_headers(headers)
+        await self.write_body(body)
